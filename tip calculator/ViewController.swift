0a7325aaa152a6b4f6eddfd50789206eaff1cf2b
Devanshi Pratiher <devanshi.pratiher@zineone.com>
2020-01-20 23:13:51 -0500

    Initial Commit

---
+++ tip calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip calculator
+//
+//  Created by Devanshi Pratiher on 1/20/20.
+//  Copyright © 2020 Devanshi Pratiher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
